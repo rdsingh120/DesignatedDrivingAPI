997b0223d9b7878b1d2b04b1e18ede5385c872a6
Ripudaman Singh <rsin1191@my.centennialcollege.ca>
2026-02-12 22:50:33 +0530

    Fix presave hook

--- models/user.model.js
+++ models/user.model.js
@@ -29,13 +29,13 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
+
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
