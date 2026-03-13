69209432caf40e662985c854490b0abf560360ac
Ripudaman Singh <rsin1191@my.centennialcollege.ca>
2026-03-13 11:03:11 +0530

    Add client url for deploying

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = express();
 
 server.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
