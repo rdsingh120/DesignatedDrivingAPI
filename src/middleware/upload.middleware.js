ef0a99b08e6816f657f469813d7ff08705901141
Ripudaman Singh <rsin1191@my.centennialcollege.ca>
2026-03-14 00:09:35 +0530

    Add upload photo to driver profile

---
+++ src/middleware/upload.middleware.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files allowed"), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+});
